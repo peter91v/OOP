3584461fa3ba5dac9c1647b8ec6775010af88f51
fhs44543 <pvarga.its-b2019@fh-salzburg.ac.at>
2020-03-17 11:40:36 +0100

    SWE1

---
+++ SWE1/SWE1Ue5/ILV/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "mal3.h"
+
+
+int main() {
+    int x = 0;
+    scanf("%d", &x);
+
+    printf("%d", mal3(x));
+
+
+    return 0;
+}
---
+++ SWE1/SWE1ue4/ILV4/Ilv/cmake-build-debug/CMakeFiles/Ilv.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ilv.dir/mystrcopy.c.obj"
+  "Ilv.exe"
+  "Ilv.exe.manifest"
+  "Ilv.pdb"
+  "libIlv.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ilv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
