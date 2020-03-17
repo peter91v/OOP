55a60a4695502a069cd88dd91b06fccfd44b5afa
fhs44543 <pvarga.its-b2019@fh-salzburg.ac.at>
2020-03-17 11:42:37 +0100

    SWE2

---
+++ SWE2/ILV01/SortFile/cmake-build-debug/CMakeFiles/SortFile.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SortFile.dir/SortFile.c.obj"
+  "SortFile.exe"
+  "SortFile.exe.manifest"
+  "SortFile.pdb"
+  "libSortFile.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SortFile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
